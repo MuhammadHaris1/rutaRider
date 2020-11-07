00465823e4a93b7974d38dbee5d51d9267deee65
Mudassirraza912 <razamudassir912@gmail.com>
2020-11-07 10:53:52 +0500

    change EndPoint to old

--- src/Constant/constant.js
+++ src/Constant/constant.js
@@ -1,5 +1,5 @@
-const API_ENDPOINT = 'http://ruta.hnhp.website/route/api/'
-
+const API_ENDPOINT = 'https://hnh6.xyz/route/api/'
+// http://ruta.hnhp.website/route/api/s
 export {
     API_ENDPOINT
 }
